85041adf8c8adb23a28afb030ab99f62fd35eed2
ohager <oliver.hager@dextra.com.br>
2015-06-23 22:31:43 -0300

    Build Pipeline

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var sequence = require('gulp-sequence');
+var jshint = require('gulp-jshint');
+var jsstylish = require('jshint-stylish');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var inject = require('gulp-inject');
+
+
+gulp.task('release', function() {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter(jsstylish))
+        .pipe(concat('sitepointsclient.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('inject', function () {
+    var target = gulp.src('./example/index.html');
+    var sources = gulp.src(['./js/**/*.js'], {read: false});
+    return  target.pipe(inject(sources))
+                    .pipe(gulp.dest('./example'));
+});
+
+gulp.task('validate', function () {
+    return  gulp.src(['./js/**/*.js'])
+                .pipe(jshint())
+                .pipe(jshint.reporter(jsstylish))
+                .pipe(gulp.dest('./js'));
+});
+
+gulp.task('default', sequence('validate', 'inject'));
+
+
---
+++ js/api.js
@@ -0,0 +1,6 @@
+/**
+ * Created by oliver on 23.06.2015.
+ */
+(function(){
+    console.log("Some things to do here!");
+})();
